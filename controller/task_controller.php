059cacc70f38119d11cf4ef05f2b0ca85e922742
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-08 19:15:19 -0300

    ADD: Create task method execute

--- controller/task_controller.php
+++ controller/task_controller.php
@@ -1,3 +1,14 @@
 <?php
 
-    
+    require "./../model/task.model.php";
+    require "./../model/task.service.php";
+    require "./../model/connection.php";
+
+    $task = new Task();
+    $task->__set('task', $_POST['description_task']);
+
+    $con = new Connection();
+
+    $taskService = new TaskService($con, $task);
+    $taskService->create();
+
--- model/task.service.php
+++ model/task.service.php
@@ -2,9 +2,21 @@
 
 class TaskService {
 
+    private $connection;
+    private $task;
+
+    public function __construct(Connection $connection, Task $task)
+    {
+        $this->connection = $connection->connect();
+        $this->task = $task;
+    }
+
     public function create()
     {
-
+        $query = 'INSERT INTO tb_tarefas(tarefa) values(:tarefa)';
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindValue(':tarefa', $this->task->__get('task'));
+        $stmt->execute();
     }
 
     public function recover()
