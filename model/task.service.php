b971571a327d9ecb7f7471b8a492b21e88dad079
SAwarm <jonasdasilvaelias@gmail.com>
2022-01-07 22:52:00 -0300

    ADD: Class TaskService

---
+++ model/task.service.php
@@ -0,0 +1,24 @@
+<?php
+
+class TaskService {
+
+    public function create()
+    {
+
+    }
+
+    public function recover()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+        
+    }
+}
